7f88034bb9871531e5d33031105604ce0cee7a60
Oliver <olivierlfb@tutamail.com>
2026-07-28 22:27:21 +0300

    Add show_menu function to display CLI options

---
+++ src/cli/menu.py
@@ -0,0 +1,10 @@
+def show_menu():
+
+    print()
+
+    print("1. Create post")
+    print("2. Add task")
+    print("3. View posts")
+    print("4. Export")
+    print("5. Statistics")
+    print("6. Exit")
